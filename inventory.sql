0995b88b2d72232427014c3207275635909e6356
dmcneary <mcneary.david@gmail.com>
2019-03-13 00:35:56 -0700

    updated DB

--- inventory.sql
+++ inventory.sql
@@ -12,14 +12,13 @@ CREATE TABLE inventory (
 );
 
 INSERT INTO inventory (product_name, department_name, retail_price, stock_qty)
-VALUES ("700c x 20/28 mm inner tube, presta valve, 48mm", 7.99, 100),
-("8 speed chain, 114 links", 19.99, 10),
-("Road brake pad inserts, Campagnolo-style, all-weather compound", 24.99, 10),
-("Seatpost clamp, 35 mm, titanium", 49.99, 2),
-("Square taper bottom bracket, ISO, 118 mm spindle", 24.99, 3),
-("Time trial helmet, white, large", 249.99, 1),
-("Carbon fiber road bike, 12-speed, 56 cm", 10999.99, 1),
-("12-inch kid's bike, orange/neon pink", 199.99, 4),
-("Chain lube, dry, 12 oz", 4.99, 20),
-("Patch kit, self-adhesive", 2.99, 62);
-
+VALUES ("700c x 20/28 mm inner tube, presta valve, 48mm", "Rubber/Tires & Tubes", 7.99, 100),
+("8 speed chain, 114 links", "Components/Drivetrain/Chains", 19.99, 10),
+("Road brake pad inserts, Campagnolo-style, all-weather compound", "Components/Brakes/Rim/Pads", 24.99, 10),
+("Seatpost clamp, 35 mm, titanium", "Components/Seatposts", 49.99, 2),
+("Square taper bottom bracket, ISO, 118 mm spindle", "Components/Bearings/Bottom Brackets", 24.99, 3),
+("Time trial helmet, white, large", "Equipment/Helmets", 249.99, 1),
+("Carbon fiber road bike, 12-speed, 56 cm", "Bicycles/Adult/Road", 10999.99, 1),
+("12-inch kid's bike, orange/neon pink", "Bicycles/Kids/12", 199.99, 4),
+("Chain lubricant, dry, 12 oz", "Lubes & Cleaners/Lubes", 4.99, 20),
+("Patch kit, self-adhesive", "Tools", 2.99, 62);
